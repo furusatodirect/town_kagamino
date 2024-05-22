fec6485fef5ab457118013be5715a014e66d3faa
mljbando <bando@mlj-inc.com>
2024-05-22 16:26:53 +0900

    20240522mlj

--- 03/js/main.js
+++ 03/js/main.js
@@ -90,6 +90,32 @@
       },
     },
   });
+
+  /**
+   * swiper product detail
+   */
+  const mySwiper_pd_thumb = new Swiper(".pd_thumb", {
+    /*slidesPerView: 5,*/
+    slidesPerView:
+      document.querySelectorAll(".pd_thumb .swiper-slide").length > 5 ? 5.5 : 5, //5枚以上の場合は初期値5→5.5枚表示
+    spaceBetween: 6,
+    grabCursor: true,
+  });
+
+  const mySwiper_pd_main = new Swiper(".pd_main", {
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true,
+    },
+    speed: 600,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: mySwiper_pd_thumb,
+    },
+  });
 })(jQuery);
 
 (function ($) {
